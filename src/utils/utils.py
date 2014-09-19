e011c73a94296d5b8bc0cffdb0f57afb29c98690
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2014-09-18 18:12:05 -0700

    Functionality to load test file

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -20,8 +20,20 @@ def load_train_data(file_name):
     return data
 
 
-def load_test_data(filename):
-    pass
+def load_test_data(file_name):
+    data = []
+    with open(file_name, 'r') as inFile:
+        for line in inFile:
+            line = line.strip('\n\r')
+            if line:
+                linesplit = line.split('\t')
+                if linesplit[0].strip() == 'PhraseId':
+                    continue
+                else:
+                    data.append({'PhraseId': linesplit[0],
+                                 'SentenceId': linesplit[1],
+                                 'Phrase': linesplit[2]})
+    return data
 
 
 def save_model(model, filename):
@@ -32,8 +44,12 @@ def load_model(filename):
     model = joblib.load(filename)
     return model
 
+
 def save_classification(data, filename):
-    pass
+    with open(filename, 'w') as outFile:
+        outFile.write('PhraseId,Sentiment\n')
+        for record in data:
+            outFile.write(record['PhraseId'] + ',' + str(record['prediction'])+'\n')
 
 
 if __name__ == '__main__':
