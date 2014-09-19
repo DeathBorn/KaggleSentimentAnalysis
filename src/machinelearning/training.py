5c98b9cf36080e41991d83bbe9829a198e981ae2
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2014-09-18 23:51:51 -0700

    Modified training to accept feature matrix.

--- src/machinelearning/training.py
+++ src/machinelearning/training.py
@@ -6,16 +6,13 @@ from numpy import array
 __author__ = 'Jasneet Sabharwal'
 
 
-def train_data(data):
-    records, labels = zip(*[(record['features'], record['Sentiment']) for
-                            record in data])
+def train_data(featureMatrix, labels):
 
     #Need to transform feature vector into something recognized by scikit learn
     #try to keep it a sparse vector so that memory utilization is low. Can we
     #do feature hashing? (what is it and how will it help?)
 
-    vec = DictVectorizer()
-    X = vec.fit_transform(records)
-    model = libsvm.fit(X.toarray(), array(labels, dtype='float64'))
+
+    model = libsvm.fit(featureMatrix.toarray(), array(labels, dtype='float64'))
 
     return model
