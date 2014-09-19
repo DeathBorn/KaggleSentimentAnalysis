47c1d31e1dc32cc3e2cf53c898a692b5398cd5b1
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2014-09-18 18:12:55 -0700

    Fixed bug caused by missing pos tags

--- src/features/featureExtraction.py
+++ src/features/featureExtraction.py
@@ -2,7 +2,7 @@ from nltk.tag.stanford import POSTagger
 from nltk.corpus import stopwords
 from collections import defaultdict
 from src.utils.sentiwordnet import SentiWordNetCorpusReader, SentiSynset
-import sys
+from collections import defaultdict
 import os
 
 __author__ = 'Jasneet Sabharwal'
@@ -21,27 +21,40 @@ def _pos_features(pos_tags):
     features = defaultdict(int)
     for (word, tag) in pos_tags:
         if 'NN' in tag:
-            features['countNoun'] += 1
+            features['countNN'] += 1
         elif 'VB' in tag:
-            features['countVerb'] += 1
+            features['countVB'] += 1
         elif 'RB' in tag:
-            features['countAdv'] += 1
+            features['countRB'] += 1
         elif 'JJ' in tag:
-            features['countAdj'] += 1
+            features['countJJ'] += 1
     return features
 
 
 def _tag_records(allTokens):
     pos_tags = POS_TAGGER.batch_tag(allTokens)
-    return pos_tags
+    posTagMap = _parse_pos_tags(pos_tags)
+    return posTagMap
+
+
+def _parse_pos_tags(allPosTags):
+    result = defaultdict(list)
+    for posTag in allPosTags:
+        words = []
+        for word, tag in posTag:
+            words.append(word)
+        result[' '.join(words)] = posTag
+    return result
 
 
 def extract_features(allTokens):
-    print "TAGGING DATA"
-    allPosTags = _tag_records(allTokens)
     result = []
+
+    print "TAGGING DATA"
+    posTagMap = _tag_records(allTokens)
+
     print "EXTRACTING FEATURES FROM TAGGED DATA"
-    for posTag in allPosTags:
-        features = _pos_features(posTag)
-        result.append(features)
+    for i, tokens in enumerate(allTokens):
+        posFeatures = _pos_features(posTagMap[' '.join(tokens)])
+        result.append(posFeatures)
     return result
