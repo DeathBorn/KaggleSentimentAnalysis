f867a79ee5957a82d2acc93ae7fb52342c60508a
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2014-09-18 18:12:32 -0700

    Added classification functionality

--- src/machinelearning/classification.py
+++ src/machinelearning/classification.py
@@ -1,5 +1,11 @@
+from sklearn.svm import libsvm
+from sklearn.feature_extraction import DictVectorizer
+
 __author__ = 'Jasneet Sabharwal'
 
 
-def classify(phrase):
-    return None
+def classify(features, model):
+    vec = DictVectorizer()
+    X = vec.fit_transform(features)
+    prediction = libsvm.predict(X.toarray(), *model)
+    return int(prediction[0])
